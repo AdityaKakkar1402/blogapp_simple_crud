aa4f6b2c0ec5642286a30e1cad70fa1d803c6613
ADITYA KAKKAR <adi@ADITYAs-MacBook-Air.local>
2023-04-04 05:11:24 +0530

    middleware

--- index.js
+++ index.js
@@ -5,9 +5,52 @@ const app = express();
 const PORT = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+//if we want to use logger everywhere
+app.use(logger);
 let blogList = [];
 
-app.get("/blogs", (req, res) => {
+// lets consider some issue with logger function then
+// by this way next function will not run and
+// from here only msg will popo up
+
+// function logger(req, res, next) {
+//   console.log(req.url);
+//   console.log(req.body);
+//   let condition = true;
+//   if (condition) {
+//     return res.status(500).json({
+//       message: "something went wrong",
+//     });
+//   }
+//   next();
+// }
+
+function logger(req, res, next) {
+  console.log(req.url);
+  console.log(req.body);
+  let condition = false;
+  if (condition) {
+    return res.status(500).json({
+      message: "something went wrong",
+    });
+  }
+  next();
+}
+
+function isAuth(req, res, next) {
+  console.log("user authed");
+  next();
+}
+
+// app.get("/blogs", logger, isAuth, (req, res) => {
+//   return res.status(200).json({
+//     data: blogList,
+//     success: true,
+//   });
+// });
+
+// islogger will run first and then isauth
+app.get("/blogs", isAuth, (req, res) => {
   return res.status(200).json({
     data: blogList,
     success: true,
